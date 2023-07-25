eaa766581ee718afe0d23c4479f9211380526542
jhuesser <github@jhuesser.ch>
2023-07-25 19:01:47 +0200

    elon jet

--- index.php
+++ index.php
@@ -5,7 +5,8 @@ $urls = array(
     "https://fortune.com/2022/05/19/elon-musk-sexual-harassment-spacex-accusation/amp/",
     "https://youtu.be/LAU1l7iEpoU",
     "https://www.cbsnews.com/miami/news/elon-musks-trans-child-wants-nothing-to-do-with-dad-billionaire-leans-toward-support-of-desantis/",
-    "https://www.businessinsider.com/elon-musk-earned-spot-at-museum-of-failure-reasons-2023-5?r=US&IR=T"
+    "https://www.businessinsider.com/elon-musk-earned-spot-at-museum-of-failure-reasons-2023-5?r=US&IR=T",
+    "https://en.wikipedia.org/wiki/ElonJet"
 );
 $randomUrl = $urls[array_rand($urls)];
 header("Location: $randomUrl");
