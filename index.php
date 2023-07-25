42d8574937b03cd440dca75ce96e964a76bb2294
Jonas Hüsser <hujo@serverbase.ch>
2023-07-25 16:49:56 +0200

    add youtube doc

--- index.php
+++ index.php
@@ -2,7 +2,8 @@
 $urls = array(
     "https://twitter.com/ParkerMolloy/status/1683229371526438912",
     "https://www.businessinsider.com/spacex-paid-250000-to-a-flight-attendant-who-accused-elon-musk-of-sexual-misconduct-2022-5?amp",
-    "https://fortune.com/2022/05/19/elon-musk-sexual-harassment-spacex-accusation/amp/"
+    "https://fortune.com/2022/05/19/elon-musk-sexual-harassment-spacex-accusation/amp/",
+    "https://youtu.be/LAU1l7iEpoU"
 );
 $randomUrl = $urls[array_rand($urls)];
 header("Location: $randomUrl");
