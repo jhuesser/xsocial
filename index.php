bc289cda9c116627827822fde83dbb7caa2ce667
jhuesser <github@jhuesser.ch>
2023-07-25 19:03:33 +0200

    twitter fails

--- index.php
+++ index.php
@@ -6,7 +6,8 @@ $urls = array(
     "https://youtu.be/LAU1l7iEpoU",
     "https://www.cbsnews.com/miami/news/elon-musks-trans-child-wants-nothing-to-do-with-dad-billionaire-leans-toward-support-of-desantis/",
     "https://www.businessinsider.com/elon-musk-earned-spot-at-museum-of-failure-reasons-2023-5?r=US&IR=T",
-    "https://en.wikipedia.org/wiki/ElonJet"
+    "https://en.wikipedia.org/wiki/ElonJet",
+    "https://edition.cnn.com/2023/03/29/media/elon-musk-twitter-promises-reliable-sources/index.html"
 );
 $randomUrl = $urls[array_rand($urls)];
 header("Location: $randomUrl");
