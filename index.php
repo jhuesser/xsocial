fa5557f5bea0f95eda67598210ff80946e877352
Jonas Hüsser <hujo@serverbase.ch>
2023-07-25 16:44:26 +0200

    interduce random links

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
 <?php
-header('Location: https://twitter.com/ParkerMolloy/status/1683229371526438912');
+$urls = array(
+    "https://twitter.com/ParkerMolloy/status/1683229371526438912",
+    "https://www.businessinsider.com/spacex-paid-250000-to-a-flight-attendant-who-accused-elon-musk-of-sexual-misconduct-2022-5?amp",
+    "https://fortune.com/2022/05/19/elon-musk-sexual-harassment-spacex-accusation/amp/"
+);
+$randomUrl = $urls[array_rand($urls)];
+header("Location: $randomUrl");
 exit();
