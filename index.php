7600eb8334fab8892a4a5b35cf58d2fdb9ae60db
jhuesser <github@jhuesser.ch>
2023-07-25 19:00:31 +0200

    museum of failure

--- index.php
+++ index.php
@@ -4,7 +4,8 @@ $urls = array(
     "https://www.businessinsider.com/spacex-paid-250000-to-a-flight-attendant-who-accused-elon-musk-of-sexual-misconduct-2022-5?amp",
     "https://fortune.com/2022/05/19/elon-musk-sexual-harassment-spacex-accusation/amp/",
     "https://youtu.be/LAU1l7iEpoU",
-    "https://www.cbsnews.com/miami/news/elon-musks-trans-child-wants-nothing-to-do-with-dad-billionaire-leans-toward-support-of-desantis/"
+    "https://www.cbsnews.com/miami/news/elon-musks-trans-child-wants-nothing-to-do-with-dad-billionaire-leans-toward-support-of-desantis/",
+    "https://www.businessinsider.com/elon-musk-earned-spot-at-museum-of-failure-reasons-2023-5?r=US&IR=T"
 );
 $randomUrl = $urls[array_rand($urls)];
 header("Location: $randomUrl");
