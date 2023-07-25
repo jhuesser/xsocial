f4b95ec6c17f71ad8aa1bf920ee6b47044db91d0
Jonas Hüsser <hujo@serverbase.ch>
2023-07-25 17:00:25 +0200

    news article regarding daughter

--- index.php
+++ index.php
@@ -3,7 +3,8 @@ $urls = array(
     "https://twitter.com/ParkerMolloy/status/1683229371526438912",
     "https://www.businessinsider.com/spacex-paid-250000-to-a-flight-attendant-who-accused-elon-musk-of-sexual-misconduct-2022-5?amp",
     "https://fortune.com/2022/05/19/elon-musk-sexual-harassment-spacex-accusation/amp/",
-    "https://youtu.be/LAU1l7iEpoU"
+    "https://youtu.be/LAU1l7iEpoU",
+    "https://www.cbsnews.com/miami/news/elon-musks-trans-child-wants-nothing-to-do-with-dad-billionaire-leans-toward-support-of-desantis/"
 );
 $randomUrl = $urls[array_rand($urls)];
 header("Location: $randomUrl");
