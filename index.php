cb62923c9abeb1cb532c1cf92d2a0d381005dca9
jhuesser <github@jhuesser.ch>
2023-07-25 19:08:15 +0200

    emerald

--- index.php
+++ index.php
@@ -9,7 +9,8 @@ $urls = array(
     "https://en.wikipedia.org/wiki/ElonJet",
     "https://edition.cnn.com/2023/03/29/media/elon-musk-twitter-promises-reliable-sources/index.html",
     "https://techcrunch.com/2023/06/14/twitter-is-being-evicted-from-its-boulder-office-over-unpaid-rent/#:~:text=Twitter%20owes%20three%20months%27%20rent,numerous%20reports%20of%20unpaid%20bills.",
-    "https://www.theguardian.com/world/2023/jun/03/twitter-conservative-media-elon-musk-ron-desantis"
+    "https://www.theguardian.com/world/2023/jun/03/twitter-conservative-media-elon-musk-ron-desantis",
+    "https://futurism.com/elon-musk-dad-emerald-mine"
 );
 $randomUrl = $urls[array_rand($urls)];
 header("Location: $randomUrl");
